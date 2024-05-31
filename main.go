6f62902072b005d46a9021620da3852ed7afc4fa
hanzalah <hanzalah@xgrid.co>
2024-06-01 04:05:52 +0500

    Added chi and logging middleware

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello", basicHandler)
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler)}
+		Handler: router,
+	}
 	if err := server.ListenAndServe(); err != nil {
 		log.Println("Failed to run server")
 	}
