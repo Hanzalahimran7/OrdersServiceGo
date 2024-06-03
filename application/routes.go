ea8ce8584c53f99f61e6682b6e648438addc3fc3
hanzalah <hanzalah@xgrid.co>
2024-06-03 13:59:48 +0500

    Added logic for handling Create and List in API

--- application/routes.go
+++ application/routes.go
@@ -1,27 +1,51 @@
 package application
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/hanzalahimran7/MicroserviceInGo/handler"
+	"github.com/hanzalahimran7/MicroserviceInGo/respository/order"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
-	router.Route("/orders", loadOrderRoutes)
-	return router
+	router.Route("/orders", a.loadOrderRoutes)
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
-	router.Post("/", orderHandler.Create)
-	router.Get("/", orderHandler.List)
+type apiFunc func(http.ResponseWriter, *http.Request) (error, int)
+
+type APIError struct {
+	Error string `json:"error"`
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+func httpHandlerFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err, status_code := f(w, r)
+		if err != nil {
+			WriteJSON(w, status_code, APIError{Error: err.Error()})
+		}
+	}
+}
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{Client: a.rdb},
+	}
+	router.Post("/", httpHandlerFunc(orderHandler.Create))
+	router.Get("/", httpHandlerFunc(orderHandler.List))
 	router.Get("/{id}", orderHandler.GetById)
 	router.Delete("/{id}", orderHandler.DeleteById)
 	router.Put("/{id}", orderHandler.PutById)
--- handler/order.go
+++ handler/order.go
@@ -1,19 +1,72 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/hanzalahimran7/MicroserviceInGo/model"
+	"github.com/hanzalahimran7/MicroserviceInGo/respository/order"
 )
 
 type Order struct {
+	Repo *order.RedisRepo
 }
 
-func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
-	log.Println("Create Order")
+func (o *Order) Create(w http.ResponseWriter, r *http.Request) (error, int) {
+	//anynomous struct for what to expect from request
+	var body struct {
+		CustomerID uuid.UUID        `json:"customer_id"`
+		LineItems  []model.LineItem `json:"line_items"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		return fmt.Errorf("INVALID REQUEST BODY"), http.StatusBadRequest
+	}
+	now := time.Now().UTC()
+	order := model.Order{
+		OrderID:    rand.Uint64(),
+		CustomerID: body.CustomerID,
+		CreatedAt:  &now,
+		LineItems:  body.LineItems,
+	}
+	if err := o.Repo.Insert(r.Context(), order); err != nil {
+		return fmt.Errorf("INTERNAL SERVER ERROR"), http.StatusInternalServerError
+	}
+	WriteJSON(w, http.StatusCreated, order)
+	return nil, 0
 }
 
-func (o *Order) List(w http.ResponseWriter, r *http.Request) {
-	log.Println("Listing Orders")
+func (o *Order) List(w http.ResponseWriter, r *http.Request) (error, int) {
+	cursor := r.URL.Query().Get("cursor")
+	if cursor == "" {
+		cursor = "0"
+	}
+	cur, err := strconv.ParseUint(cursor, 10, 64)
+	if err != nil {
+		return fmt.Errorf("INVALID CURSOR: %v", cursor), http.StatusBadRequest
+	}
+	const size = 50
+	res, err := o.Repo.ListOrders(r.Context(), order.FindAllPage{
+		Offset: uint(cur),
+		Size:   size,
+	})
+	if err != nil {
+		return fmt.Errorf("INTERNAL SERVER ERROR"), http.StatusInternalServerError
+	}
+	var body struct {
+		Items []model.Order `json:"items"`
+		Next  uint64        `json:"next,omitempty"`
+	}
+	body.Items = res.Orders
+	body.Next = res.Cursor
+	WriteJSON(w, http.StatusCreated, body)
+	return nil, 0
+
 }
 
 func (o *Order) GetById(w http.ResponseWriter, r *http.Request) {
@@ -26,4 +79,10 @@ func (o *Order) PutById(w http.ResponseWriter, r *http.Request) {
 
 func (o *Order) DeleteById(w http.ResponseWriter, r *http.Request) {
 	log.Println("Delete Order by ID")
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
 }
